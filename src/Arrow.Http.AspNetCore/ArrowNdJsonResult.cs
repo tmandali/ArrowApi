90ad550fbd0ec7e30714f827c2529a47ec80b888
TIMUR MANDALI <TIMUR.MANDALI+lcw@Lcwaikiki.com>
2026-08-01 18:38:48 +0300

    http test clean jobb endpoints

--- src/Arrow.Http.AspNetCore/ArrowNdJsonResult.cs
+++ src/Arrow.Http.AspNetCore/ArrowNdJsonResult.cs
@@ -1,21 +1,26 @@
 using Arrow.Data;
 using Microsoft.AspNetCore.Http;
+using System.Text.Encodings.Web;
 using System.Text.Json;
 
 namespace Arrow.Http.AspNetCore;
 
 /// <summary>
-/// <see cref="ArrowBatchReader"/> nesnesini <c>application/x-ndjson</c> akışı olarak HTTP yanıtına yazar.
+/// <see cref="ArrowBatchReader"/> nesnesini <c>application/x-ndjson; charset=utf-8</c> akışı olarak HTTP yanıtına yazar.
 /// </summary>
 internal sealed class ArrowNdJsonResult(ArrowBatchReader reader, bool disposeReader = true) : IResult
 {
     private static readonly byte[] NewlineBytes = "\n"u8.ToArray();
+    private static readonly JsonSerializerOptions _jsonOptions = new JsonSerializerOptions
+    {
+        Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+    };
 
     public async Task ExecuteAsync(HttpContext httpContext)
     {
         if (httpContext is null) throw new ArgumentNullException(nameof(httpContext));
 
-        httpContext.Response.ContentType = "application/x-ndjson";
+        httpContext.Response.ContentType = "application/x-ndjson; charset=utf-8";
 
         try
         {
@@ -28,7 +33,7 @@ internal sealed class ArrowNdJsonResult(ArrowBatchReader reader, bool disposeRea
                     row[dbReader.GetName(i)] = dbReader.IsDBNull(i) ? null : dbReader.GetValue(i);
                 }
 
-                await JsonSerializer.SerializeAsync(httpContext.Response.Body, row, cancellationToken: httpContext.RequestAborted).ConfigureAwait(false);
+                await JsonSerializer.SerializeAsync(httpContext.Response.Body, row, _jsonOptions, httpContext.RequestAborted).ConfigureAwait(false);
                 await httpContext.Response.Body.WriteAsync(NewlineBytes, httpContext.RequestAborted).ConfigureAwait(false);
             }
 
